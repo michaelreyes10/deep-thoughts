bf3056184a862f7bce53c8561dbe2a5ac103d268
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-11-10 13:50:00 -0500

    client side working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS } from '../utils/queries';
-
+import ThoughtList from '../components/ThoughtList';
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -12,10 +12,16 @@ console.log(thoughts);
 
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
-      </div>
-    </main>
+  <div className="flex-row justify-space-between">
+    <div className="col-12 mb-3">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+      )}
+    </div>
+  </div>
+</main>
   );
 };
 
